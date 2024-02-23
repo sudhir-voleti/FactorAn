62e5e1a74fbac2635ac53689baf49355268f172c
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-02-23 15:53:54 +0530

    Update server.R

--- server.R
+++ server.R
@@ -78,7 +78,7 @@ output$fxvarselect <- renderUI({
 # Create dummy variables wala final DF
 filtered_dataset <- reactive({
 	#dummy_vars <- lapply(Dataset1()[input$fxAttr], function(x) model.matrix(~ x - 1))
-	dummy_vars = fastDummies::dummy_cols(Dataset1(), select_columns = input$fxAttr, 
+	dummy_vars = fastDummies::dummy_cols(Dataset1(), select_columns = c(input$fxAttr), 
 					     remove_first_dummy = TRUE, remove_selected_columns = TRUE)
 	
 	df1 <- Dataset1()[, input$selVar]
