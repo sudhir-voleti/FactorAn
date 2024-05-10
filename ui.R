edc8a56cc05a6011517e6b501230783cf59c4e03
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-05-10 13:35:20 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -65,8 +65,8 @@ h4(p("Download Sample Input File")),
 downloadButton('downloadData', 'Download Big 5 Survey sample file'), br(),
 downloadButton('downloadData2', 'Download mtcars sample file'), br(),
 downloadButton('downloadData3', 'Download toothpaste survey sample file'), br(),
-downloadButton('downloadData4', 'Download toothpaste survey sample file'), br(),
-downloadButton('downloadData5', 'Download toothpaste survey sample file'), br(),
+downloadButton('downloadData4', 'Download diamonds prices sample file'), br(),
+downloadButton('downloadData5', 'Download spotify audio features sample file'), br(),
 p("Please note that download will not work with RStudio interface. Download will work only in web-browsers. So open this app in a web-browser and then download the example file. For opening this app in web-browser click on \"Open in Browser\" as shown below -"),
 img(src = "example1.png") #, height = 280, width = 400
 
